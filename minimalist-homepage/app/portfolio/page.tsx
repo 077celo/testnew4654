f68b0cf484235ba6bb88b6f994cc3ed9e98e3ab6
077celo <marcelob465@gmail.com>
2025-07-16 21:10:56 -0300

    Update page.tsx

--- minimalist-homepage/app/portfolio/page.tsx
+++ minimalist-homepage/app/portfolio/page.tsx
@@ -164,7 +164,7 @@ export default function Portfolio() {
               <div className="absolute -inset-4 rounded-xl bg-gradient-to-r from-green-400/20 to-cyan-700/20 blur-xl opacity-70"></div>
               <div className="relative aspect-square rounded-xl border border-zinc-800">
                 <img
-                  src="https://cdna.artstation.com/p/assets/images/images/068/104/244/large/marcelo-b-highresscreenshot00000.jpg?1696995672?height=600&width=600"
+                  src="https://cdnb.artstation.com/p/assets/images/images/090/042/723/large/marcelo-b-dither-shader.jpg?1752710975?1696995672?height=600&width=600"
                   alt="Marcelo Bensabath"
                   className="w-full h-full object-cover"
                 />
